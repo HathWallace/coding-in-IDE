268891a903be4e1b843bd8a4077f17ace3c6810c
HathWallace <hathzhou@qq.com>
2021-07-17 09:34:42 +0800

    VS2019初始化

---
+++ VS2019/UnitTestProject/SolutionTest.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var solution = new Solution();
+        }
+    }
+}
